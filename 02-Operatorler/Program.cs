1efb91fadac4669b90125ec54f2e5c7413203d10
Gizem Bilgiç <gizemmbilgicc@gmail.com>
2025-02-10 00:35:09 +0300

    Operatörler ile ilgili örnekler eklendi.

---
+++ 02-Operatorler/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _02_Operatorler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** İlişkisel Operatörler ***");
+
+            int a = 5;
+            int b = 10;
+            Console.WriteLine(a > b);  // False
+            Console.WriteLine(a < b);  // True
+            Console.WriteLine(a != b); // True
+            Console.WriteLine(a == b); // False
+            Console.WriteLine(a <= b); // True
+            Console.WriteLine(a >= b); // False
+
+
+            Console.WriteLine("*** Atama Operatörü ***");
+            int x = 5;
+            x += 3; // x artık 8 oldu!
+            Console.WriteLine(x);
+
+
+            Console.WriteLine("*** Aritmetik Operatörler ***");
+            int sayi = 10;
+            Console.WriteLine(sayi + 5);  // 15
+            Console.WriteLine(sayi % 3);  // 1 (kalan)
+            sayi++;
+            Console.WriteLine(sayi);      // 11
+
+
+            Console.WriteLine("*** Mantıksal Operatörler ***");
+            bool durum = (10 > 5) && (5 < 3); // False
+            Console.WriteLine(!durum);       // True
+
+
+            Console.WriteLine("*** Ternary Operatörü ***");
+            int number = 15;
+            string mesaj = (number < 10) ? "Sayı 10'dan küçük" : "Sayı 10 veya daha büyük";
+            Console.WriteLine(mesaj);
+        }
+    }
+}
