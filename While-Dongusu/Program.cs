963bf3a0370e3d2dc6e16ac2078e05a6b49bae8e
Gizem Bilgiç <gizemmbilgicc@gmail.com>
2025-02-10 00:45:00 +0300

    While döngüsü ile ilgili örnek sorular eklendi.

---
+++ While-Dongusu/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace While_Dongusu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                1. 1'den 10'a kadar olan sayıları ekrana yazdıran bir while döngüsü yazın.
+                2. Kullanıcıdan bir sayı alarak, 1'den bu sayıya kadar olan çift sayıları ekrana yazdıran bir while döngüsü yazın.
+                3. 1'den 100'e kadar olan sayıların toplamını hesaplayan bir while döngüsü yazın.
+                4. Kullanıcıdan bir sayı alarak, bu sayının faktöriyelini hesaplayan bir while döngüsü yazın.
+                5. Kullanıcıdan bir metin alıp, bu metni while döngüsü kullanarak tersten yazdırın.
+                6. 1 ile 50 arasındaki tek sayıların toplamını hesaplayan bir while döngüsü yazın.
+                7. Girilen bir sayının asal olup olmadığını kontrol eden bir while döngüsü yazın.
+                8. Kullanıcıdan sürekli olarak sayı alıp, toplamları 100’ü geçene kadar devam eden bir while döngüsü yazın.
+                9. Bir dizi içerisindeki en büyük elemanı bulan bir while döngüsü yazın.
+               10. Kullanıcının belirttiği satır sayısına göre yıldızlardan oluşan bir üçgen şekli çizen bir while döngüsü yazın. (Örnek: 5 satır için)
+                    *
+                    **
+                    ***
+                    ****
+                    *****            
+             */
+        }
+    }
+}
