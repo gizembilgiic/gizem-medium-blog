d614c27119f655f1e4a37206d8beafe15d6516c4
Gizem Bilgiç <gizemmbilgicc@gmail.com>
2025-02-10 00:46:00 +0300

    Do-While döngüsü ile ilgili örnek sorular eklendi.

---
+++ DoWhile-Dongusu/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace DoWhile_Dongusu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                1. 1'den 10'a kadar olan sayıları ekrana yazdıran bir do-while döngüsü yazın.
+                2. Kullanıcıdan bir sayı alarak, 1'den bu sayıya kadar olan çift sayıları ekrana yazdıran bir do-while döngüsü yazın.
+                3. 1'den 100'e kadar olan sayıların toplamını hesaplayan bir do-while döngüsü yazın.
+                4. Kullanıcıdan bir sayı alarak, bu sayının faktöriyelini hesaplayan bir do-while döngüsü yazın.
+                5. Kullanıcıdan bir kelime alıp, bu kelimeyi do-while döngüsü kullanarak harflerini alt alta yazdıran bir program yazın.
+                6. 1 ile 50 arasındaki tek sayıların toplamını hesaplayan bir do-while döngüsü yazın.
+                7. Girilen bir sayının asal olup olmadığını kontrol eden bir do-while döngüsü yazın.
+                8. Kullanıcıdan sürekli olarak sayı alıp, toplamları 100’ü geçene kadar devam eden bir do-while döngüsü yazın.
+                9. Kullanıcıdan şifre alıp doğru şifre girilene kadar tekrar isteyen bir do-while döngüsü yazın. (Örn: Doğru şifre "1234" ise, kullanıcı doğru şifreyi girene kadar şifre sormaya devam etsin.)
+               10. Kullanıcının belirttiği satır sayısına göre yıldızlardan oluşan bir üçgen şekli çizen bir do-while döngüsü yazın. (Örnek: 5 satır için)
+
+                    *
+                    **
+                    ***
+                    ****
+                    *****            
+             */
+        }
+    }
+}
