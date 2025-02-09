4d97d0aa0cd7e3c3e79dcdbb21f2306bd1b19da7
Gizem Bilgiç <gizemmbilgicc@gmail.com>
2025-02-10 00:48:15 +0300

    Foreach döngüsü ile ilgili örnek sorular eklendi.

---
+++ Foreach-Dongusu/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Foreach_Dongusu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                1. Bir dizide bulunan tüm elemanları ekrana yazdıran bir foreach döngüsü yazın.
+                2. Bir liste içinde bulunan şehir isimlerini foreach döngüsü kullanarak ekrana yazdıran bir program yazın.
+                3. Bir dizide bulunan sayıların toplamını foreach döngüsü kullanarak hesaplayan bir program yazın.
+                4. Bir string dizisinde yer alan kelimeleri büyük harfe çevirip ekrana yazdıran bir foreach döngüsü yazın.
+                5. Bir dizideki sayıların karesini alıp ekrana yazdıran bir foreach döngüsü yazın.
+                6. Bir kelimenin karakterlerini foreach döngüsü ile ekrana yazdıran bir program yazın.
+                7. Bir listede bulunan çift sayıları ekrana yazdıran bir foreach döngüsü yazın.
+                8. Bir dizideki en büyük ve en küçük sayıyı bulan bir foreach döngüsü yazın.
+                9. Bir öğrencinin aldığı notları içeren bir listeyi foreach döngüsü ile dolaşıp, eğer not 50'den düşükse "Başarısız" diğer durumlarda "Başarılı" yazdıran bir program yazın.
+               10. Bir dictionary (sözlük) yapısı kullanarak, öğrencilerin isimleri ve notlarını saklayan ve bu bilgileri foreach döngüsü ile ekrana yazdıran bir program yazın.
+             */
+        }
+    }
+}
